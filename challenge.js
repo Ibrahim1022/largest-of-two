60f115b4e7dffd8e8088bbce3b9b9663aa4f6b0b
Ibrahim1022 <ibrahimhusseinjama1995@gmail.com>
2024-08-18 19:45:42 +0300

    Done excercise

--- challenge.js
+++ challenge.js
@@ -1,2 +1,14 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+
+
+let num1 = 10;
+let num2 = 20;
+
+if (num1 > num2) {
+    console.log(`The largest number out of the two numbers is ${num1}`);
+} else if (num2 > num1) {
+    console.log(`The largest number out of the two numbers is ${num2}`);
+} else {
+    console.log('Both numbers are equal');
+}
